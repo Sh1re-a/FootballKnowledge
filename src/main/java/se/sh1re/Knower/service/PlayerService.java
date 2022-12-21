b2d6e68f133051f827a926687747da73c772b789
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-12-21 22:48:05 +0100

    PlayerService is class and test is done

--- src/main/java/se/sh1re/Knower/service/PlayerService.java
+++ src/main/java/se/sh1re/Knower/service/PlayerService.java
@@ -2,34 +2,78 @@ package se.sh1re.Knower.service;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.safari.SafariDriver;
-import se.sh1re.Knower.models.Player;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class PlayerService {
-
-    private Player player;
-
-
     private static final String safariWebDriver = "webdriver.safari.driver";
     private static final String safariWebDriverPath = "/usr/bin/safaridriver";
     private static SafariDriver safariDriver;
 
 
-
     public void initializeDriver(String player) {
         System.setProperty(safariWebDriver, safariWebDriverPath);
         safariDriver = new SafariDriver();
-        safariDriver.navigate().to(player);
+        safariDriver.navigate().to("https://en.wikipedia.org/wiki/" + player);
         safariDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
 
 
-
-    public String getPersonName(String className) {
-        String name = safariDriver.findElement(new By.ByXPath("/html[@class='hydrated']/body/div[@id='main']/main/div[@class='row']/div[@class='large-8 columns']/div[@class='box viewport-tracking'][1]/div[@class='row collapse']/div[@class='large-6 large-pull-6 small-12 columns spielerdatenundfakten']/div[@class='info-table info-table--right-space']/span[@class='info-table__content info-table__content--bold'][1]")).getText();
-        return name;
+    public String getPersonName() {
+        String personNameXPath = "//*[@id=\"firstHeading\"]/span";
+        String personName = safariDriver.findElement(By.xpath(personNameXPath)).getText();
+        return personName;
     }
+
+    public String getPersonFullName() {
+        String personFullNameXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[3]/td";
+        String personFullName = safariDriver.findElement(By.xpath(personFullNameXPath)).getText();
+        return personFullName;
+    }
+    public String getPersonAgeAndBirthDate() {
+        String ageAndBirthXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[4]/td";
+        String personAgeAndBirth = safariDriver.findElement(By.xpath(ageAndBirthXPath)).getText();
+        return personAgeAndBirth;
+    }
+
+    public String getPlayerHeight() {
+        String playerHeightsXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[6]/td";
+        String playersHeight = safariDriver.findElement(By.xpath(playerHeightsXPath)).getText();
+        return playersHeight;
+    }
+
+    public String getPlayersPositions() {
+        String playersPositionsXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[7]/td";
+        String playersPositions = safariDriver.findElement(By.xpath(playersPositionsXPath)).getText();
+        return playersPositions;
+    }
+
+    
+    public String getPlayersBirthOfPlace() {
+        String playerBirthOfPlaceXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[5]/td";
+        String playersBirthOfPlace = safariDriver.findElement(By.xpath(playerBirthOfPlaceXPath)).getText();
+        return playersBirthOfPlace;
+    }
+
+    
+    public String getPlayersCurrentClub() {
+        String checkIfPlayerHasACurrentClub = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[8]/th";
+        String expectedInformation = "Club information";
+        String text = safariDriver.findElement(By.xpath(checkIfPlayerHasACurrentClub)).getText();
+
+        if(text.replaceAll("\\s+", "").equals(expectedInformation.replaceAll("\\s+", ""))) {
+            String clubPath = "//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[9]/td";
+            String expectedClub = "ParisSaint-Germain";
+            String club = safariDriver.findElement(By.xpath(clubPath)).getText();
+            return club;
+        }
+        else {
+            return null;
+        }
+
+    }
+
 
 
 
