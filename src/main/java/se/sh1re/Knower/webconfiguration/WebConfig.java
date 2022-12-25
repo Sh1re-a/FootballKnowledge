19a31ce8616acedba1487b417a4313ba3f566475
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-12-25 05:04:01 +0100

    Can get a player through API

--- src/main/java/se/sh1re/Knower/webconfiguration/WebConfig.java
+++ src/main/java/se/sh1re/Knower/webconfiguration/WebConfig.java
@@ -1,4 +1,4 @@
-package com.group8.bookingapp.webconfiguration;
+package se.sh1re.Knower.webconfiguration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
