789b034ac1b16bf68de394c1db4aea8251514dee
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-12-26 08:51:09 +0100

    Added a function, where u can mass search after player with a String[]

--- Backend/src/main/java/se/sh1re/Knower/controller/playerController.java
+++ Backend/src/main/java/se/sh1re/Knower/controller/playerController.java
@@ -1,5 +1,6 @@
 package se.sh1re.Knower.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.SessionNotCreatedException;
@@ -18,8 +19,11 @@ import se.sh1re.Knower.models.model.Player;
 import se.sh1re.Knower.models.repository.PlayerRepo;
 import se.sh1re.Knower.service.PathValidatorService;
 import se.sh1re.Knower.service.PlayerService;
+import springfox.documentation.spring.web.json.Json;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 @RestController
 public class playerController {
@@ -27,7 +31,7 @@ public class playerController {
     @Autowired
     private PlayerService playerService;
 
-
+    private Safari safari;
     @Autowired
     private PlayerRepo playerRepo;
     @Autowired
@@ -40,108 +44,212 @@ public class playerController {
 
     @PostMapping(value="/search")
     public ResponseEntity<Player>getPlayer(@RequestBody String playerName) throws  InterruptedException {
-       boolean StartSelenium = false;
-
-        String dbName = playerName;
-        dbName = dbName.replaceAll("_"," ");
 
 
+            boolean StartSelenium = false;
 
-       if(playerRepo.findByName(dbName) == null){
-           StartSelenium = true;
-       }
-       else {
-
-          Player DatebasePlayer = playerRepo.findByName(dbName);
-          DatebasePlayer.setPositions(playerService.getPlayersPositions(DatebasePlayer.getDatabasePositions()));
-          DatebasePlayer.setPlaceOfBirth(playerService.getPlayersBirthOfPlace(DatebasePlayer.getDatabasePlaceOfBirth()));
-
-           return new ResponseEntity<>(DatebasePlayer,HttpStatus.OK);
-       }
-        Safari safari = new Safari();
-    while (StartSelenium)
-        try {
-        System.setProperty(safariWebDriver, safariWebDriverPath);
-        playerName = playerName.strip();
-
-        pathValidatorService = new PathValidatorService();
-
-        playerService = new PlayerService();
-        safari.getDriver().navigate().to("https://en.wikipedia.org/wiki/" + playerName);
-        safari.getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        Player player = new Player(
-                playerService.getPersonName(safari.getDriver().
-                findElement(By.xpath(XPath.name.toString())).getText()),
-
-                playerService.getPersonFullName(safari.getDriver()
-                .findElement(By.xpath(pathValidatorService.validateInformation
-                        (PathValidator.FullName.toString(),safari))).getText()),
-
-                playerService.getPlayersBirthOfPlace(safari.getDriver()
-                    .findElement(By.xpath(pathValidatorService.validateInformation
-                                    (PathValidator.PlaceOfBirth.toString(),safari))).getText()),
-
-                playerService.getPersonBirthDate(safari.getDriver().
-                    findElement(By.xpath(pathValidatorService.validateInformation
-                                    (PathValidator.DateOfBirth.toString(),safari))).getText()), //Tar lång tid
-
-                playerService.getPersonAge(playerService.getPersonBirthDate(safari.getDriver().
-                        findElement(By.xpath(pathValidatorService.validateInformation
-                                        (PathValidator.DateOfBirth.toString(),safari))).getText())), //Tar lång tid
+            String dbName = playerName;
+            dbName = dbName.replaceAll("_", " ");
 
 
-                playerService.getPlayerHeight(safari.getDriver()
-                    .findElement(By.xpath(pathValidatorService.validateInformation
-                                    (PathValidator.Height.toString(),safari))).getText()),
+            if (playerRepo.findByName(dbName) == null) {
+                StartSelenium = true;
+            } else {
 
-                playerService.getPlayersCurrentClub(safari.getDriver()
-                    .findElement(By.xpath(pathValidatorService.validateInformation
-                                    (PathValidator.CurrentClub.toString(),safari))).getText()),
+                Player DatebasePlayer = playerRepo.findByName(dbName);
+                DatebasePlayer.setPositions(playerService.getPlayersPositions(DatebasePlayer.getDatabasePositions()));
+                DatebasePlayer.setPlaceOfBirth(playerService.getPlayersBirthOfPlace(DatebasePlayer.getDatabasePlaceOfBirth()));
 
-                playerService.getPlayersPositions(safari.getDriver()
-                .findElement(By.xpath(pathValidatorService.validateInformation
-                                (PathValidator.Positions.toString(),safari))).getText()),
-
-
-                playerService.getPlayersShirtNumber(safari.getDriver()
-                .findElement(By.xpath(pathValidatorService.validateInformation
-                                (PathValidator.ShirtNumber.toString(),safari))).getText()));
+                return new ResponseEntity<>(DatebasePlayer, HttpStatus.OK);
+            }
 
 
+            while (StartSelenium)
+                try {
+                    safari = new Safari();
+                    System.setProperty(safariWebDriver, safariWebDriverPath);
+                    playerName = playerName.strip();
 
-        safari.tearDown();
+                    pathValidatorService = new PathValidatorService();
 
-        Player playerDB = player;
+                    playerService = new PlayerService();
+                    safari.getDriver().navigate().to("https://en.wikipedia.org/wiki/" + playerName);
+                    safari.getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+                    Player player = new Player(
+                            playerService.getPersonName(safari.getDriver().
+                                    findElement(By.xpath(XPath.name.toString())).getText()),
+
+                            playerService.getPersonFullName(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.FullName.toString(), safari))).getText()),
+
+                            playerService.getPlayersBirthOfPlace(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.PlaceOfBirth.toString(), safari))).getText()),
+
+                            playerService.getPersonBirthDate(safari.getDriver().
+                                    findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.DateOfBirth.toString(), safari))).getText()), //Tar lång tid
+
+                            playerService.getPersonAge(playerService.getPersonBirthDate(safari.getDriver().
+                                    findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.DateOfBirth.toString(), safari))).getText())), //Tar lång tid
 
 
-        playerDB.setDatabasePlaceOfBirth(playerService.setArrayToStringForDB(player.getPlaceOfBirth()));
-        playerDB.setDatabasePositions(playerService.setArrayToStringForDB(player.getPositions()));
-        playerRepo.save(playerDB);
-        return new ResponseEntity<>(player, HttpStatus.OK);
+                            playerService.getPlayerHeight(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.Height.toString(), safari))).getText()),
 
+                            playerService.getPlayersCurrentClub(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.CurrentClub.toString(), safari))).getText()),
+
+                            playerService.getPlayersPositions(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.Positions.toString(), safari))).getText()),
+
+
+                            playerService.getPlayersShirtNumber(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.ShirtNumber.toString(), safari))).getText()));
+
+
+                    safari.tearDown();
+
+                    Player playerDB = player;
+
+
+                    playerDB.setDatabasePlaceOfBirth(playerService.setArrayToStringForDB(player.getPlaceOfBirth()));
+                    playerDB.setDatabasePositions(playerService.setArrayToStringForDB(player.getPositions()));
+                    playerRepo.save(playerDB);
+                    return new ResponseEntity<>(player, HttpStatus.OK);
+
+                } catch (SessionNotCreatedException | UnreachableBrowserException e) {
+                    safari.tearDown();
+                    Thread.sleep(2000);
+
+                    continue;
+                } catch (NoSuchElementException e) {
+                    System.out.println("No such element was found, check the if the name was spelled correct");
+                    safari.tearDown();
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                } catch (TimeoutException e) {
+                    safari.tearDown();
+                    Thread.sleep(2000);
+
+                    continue;
+                }
+
+
+
+        return null;
     }
-        catch (SessionNotCreatedException | UnreachableBrowserException e ) {
-            safari.tearDown();
-            Thread.sleep(2000);
 
-            continue;
+    @PostMapping(value="/search1")
+    public List<Player> getPlayers(@RequestBody String[] playerName) throws  InterruptedException {
+        List<Player> players = new ArrayList<>(playerName.length);
+        for(int i = 0; i < playerName.length; i++) {
+
+            boolean StartSelenium = false;
+
+            String dbName = playerName[i];
+            dbName = dbName.replaceAll("_", " ");
+
+
+            if (playerRepo.findByName(dbName) == null) {
+                StartSelenium = true;
+            } else {
+
+                Player DatebasePlayer = playerRepo.findByName(dbName);
+                DatebasePlayer.setPositions(playerService.getPlayersPositions(DatebasePlayer.getDatabasePositions()));
+                DatebasePlayer.setPlaceOfBirth(playerService.getPlayersBirthOfPlace(DatebasePlayer.getDatabasePlaceOfBirth()));
+
+                players.add(DatebasePlayer);
+            }
+
+            while (StartSelenium)
+
+                try {
+                    safari = new Safari();
+                    System.setProperty(safariWebDriver, safariWebDriverPath);
+                    playerName[i] = playerName[i].strip();
+
+                    pathValidatorService = new PathValidatorService();
+
+                    playerService = new PlayerService();
+                    safari.getDriver().navigate().to("https://en.wikipedia.org/wiki/" + playerName[i]);
+                    safari.getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+                    Player player = new Player(
+                            playerService.getPersonName(safari.getDriver().
+                                    findElement(By.xpath(XPath.name.toString())).getText()),
+
+                            playerService.getPersonFullName(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.FullName.toString(), safari))).getText()),
+
+                            playerService.getPlayersBirthOfPlace(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.PlaceOfBirth.toString(), safari))).getText()),
+
+                            playerService.getPersonBirthDate(safari.getDriver().
+                                    findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.DateOfBirth.toString(), safari))).getText()), //Tar lång tid
+
+                            playerService.getPersonAge(playerService.getPersonBirthDate(safari.getDriver().
+                                    findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.DateOfBirth.toString(), safari))).getText())), //Tar lång tid
+
+
+                            playerService.getPlayerHeight(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.Height.toString(), safari))).getText()),
+
+                            playerService.getPlayersCurrentClub(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.CurrentClub.toString(), safari))).getText()),
+
+                            playerService.getPlayersPositions(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.Positions.toString(), safari))).getText()),
+
+
+                            playerService.getPlayersShirtNumber(safari.getDriver()
+                                    .findElement(By.xpath(pathValidatorService.validateInformation
+                                            (PathValidator.ShirtNumber.toString(), safari))).getText()));
+
+
+                    safari.tearDown();
+
+                    Player playerDB = player;
+
+
+                    playerDB.setDatabasePlaceOfBirth(playerService.setArrayToStringForDB(player.getPlaceOfBirth()));
+                    playerDB.setDatabasePositions(playerService.setArrayToStringForDB(player.getPositions()));
+                    playerRepo.save(playerDB);
+                    players.add(player);
+                    break;
+
+                } catch (SessionNotCreatedException | UnreachableBrowserException e) {
+                    safari.tearDown();
+                    Thread.sleep(2000);
+
+                    continue;
+                } catch (NoSuchElementException e) {
+                    System.out.println("No such element was found, check the if the name was spelled correct");
+                    safari.tearDown();
+                    break;
+                } catch (TimeoutException e) {
+                    safari.tearDown();
+                    Thread.sleep(2000);
+
+                    continue;
+                }
+
+
         }
+        return players;
 
-        catch (NoSuchElementException e){
-            System.out.println("No such element was found, check the if the name was spelled correct");
-            safari.tearDown();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
 
-    catch (TimeoutException e) {
-        safari.tearDown();
-        Thread.sleep(2000);
-
-        continue;
     }
-
-    return null;
-    }
-
 
 }
