799100939e7c712bc405c4ed57848fc6a29eaffc
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-12-26 06:43:26 +0100

    Instead of searching on the web once a player have been searched he is saved in the database

--- Backend/src/main/java/se/sh1re/Knower/models/repository/PlayerRepo.java
+++ Backend/src/main/java/se/sh1re/Knower/models/repository/PlayerRepo.java
@@ -2,9 +2,15 @@ package se.sh1re.Knower.models.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.sh1re.Knower.models.model.Player;
 
 @Repository
 public interface PlayerRepo extends JpaRepository<Player, Long> {
+
+
+    Player findByName(String name);
+
 }
