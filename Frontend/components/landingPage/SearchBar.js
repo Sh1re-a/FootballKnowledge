954978b453ffd73f1810298e568c9cfcac28bd8b
fethi9an <martinfethi97@hotmail.com>
2023-05-16 17:01:55 +0200

    fixed so when you push the button you dont need to do it twice

--- Frontend/components/landingPage/SearchBar.js
+++ Frontend/components/landingPage/SearchBar.js
@@ -2,35 +2,29 @@ import React, { useState } from 'react'
 import styles from './SearchBar.module.css'
 import { FaSearch } from 'react-icons/fa'
 
-export const SearchBar = ({player}) => {
-  const [playerName, setPlayerName] = useState("");
-  const [inputText, setInputText] = useState("");
-  
+export const SearchBar = ({ player }) => {
+  const [inputText, setInputText] = useState('');
 
   const handleClick = () => {
-  const transformed = inputText
-  .split(' ')
-  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-  .join('_');
+    const transformed = inputText
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join('_');
 
-  setPlayerName(transformed)
-  sendDataToParent();
-  console.log(transformed);
-
-    
+    player(transformed);
+    console.log(transformed);
   }
 
-  const sendDataToParent = () => {
-    player(playerName);
-  }
   return (
     <div className={styles.SearchBar}>
       <div style={{ position: 'relative' }}>
-        <input placeholder='Lionel Messi'
-        type="text"
-        value={inputText}
-        onChange={e => setInputText(e.target.value)}
-        className={styles.searchInput} />
+        <input
+          placeholder='Lionel Messi'
+          type="text"
+          value={inputText}
+          onChange={e => setInputText(e.target.value)}
+          className={styles.searchInput}
+        />
         <FaSearch
           onClick={handleClick}
           style={{
